a612a897997dcca3ee757835d1a5f8a9fd9bc972
ogog21 <enzo.candotti16@hotmail.com>
2019-02-01 14:21:27 +0100

    Pofinage

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
     $bdd = new PDO('mysql:host=localhost;dbname=entrainement','root','password');
     if($bdd){
-        echo 'Base de donnée connecté <br />';
+        $infoBD = 'Base de donnée connecté <br />';
     }else {
-        echo 'Probleme avec la connexion à la base de donnée <br />';
+        $infoBD = 'Probleme avec la connexion à la base de donnée <br />';
     }
     if(!empty($_POST['pseudo']) AND !empty($_POST['message'])){
         $messages = htmlspecialchars($_POST['message']);
@@ -11,13 +11,13 @@
 
         $insterMSG = $bdd->prepare('INSERT INTO messagerie(pseudo, message) VALUES(?,?)');
         $insterMSG->execute(array($pseudosecure,$messages));
-        echo 'Message envoyé avec succès <br />';
+        $Rchamp = 'Message envoyé avec succès <br />';
     }else{
-        echo 'Remplissez tous les champs ! <br />';
+        $Rchamp = 'Remplissez tous les champs ! <br />';
     }
     if(isset($_POST['clear'])){
         $bdd->exec('DELETE FROM messagerie');
-        echo 'Supprimer';
+        $clear = 'Supprimer <br />';
     }
 ?>
 <!DOCTYPE html>
@@ -31,10 +31,25 @@
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
 </head>
 <body>
+    
+    <div id='block_actu'>
+        <p id='actu'>Actu de la page</p>
+        <?php
+            if(isset($Rchamp)){
+                echo $Rchamp;
+            }
+            if(isset($clear)){
+                echo $clear;
+            }
+            if(isset($infoBD)){
+                echo $infoBD;
+            }
+        ?>
+    </div>
     <h1 style='text-align:center;'>Chat en PHP w/ jQuery</h1>
     <div id="main">
         <form method='POST' action=''><input type='submit' name='clear' value='Clear' id='clear_button'></form>
-    <p style='margin-left:50px;'><u>Messages postés :</u></p>
+        <p style='margin-left:50px;'><u>Messages postés :</u></p>
         <div id="post">
             <?php
                 $bdd = new PDO('mysql:host=localhost;dbname=entrainement','root','password');
@@ -46,7 +61,7 @@
         </div>
         <div id='envoie'>
             <form method='POST' action='' id='input_center'>
-                <label>Pseudo :</label><input type='text' placeholder='votre pseudo' name='pseudo'><br />
+                <label>Pseudo :</label><input type='text' placeholder='votre pseudo' name='pseudo' value="<?php if(isset($pseudosecure)){echo $pseudosecure;}?>" ><br />
                 <label>Votre messages :</label><textarea name='message' placeholder='votre message' id="message"></textarea><br />
                 <input type='submit' name="envoie" value="Envoyer">
             </form>
